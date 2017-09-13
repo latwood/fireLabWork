7f2a494ef41756d7a78d0a1f017ad4654702b562
latwood <pianotocador@gmail.com>
2017-09-13 13:48:04 -0600

    Changed the lapseRateProfileTemperatureInlet to a more easy to use base. Probably going to have to halt this idea again too since it still isn't the best match to what I am doing. May be a scalar field instead of a vector field, but now it is meant for walls

--- C++/OpenFOAM/utilities/lapseRateProfileTemperatureInlet/lapseRateProfileTemperatureInletFvPatchScalarField.C
+++ C++/OpenFOAM/utilities/lapseRateProfileTemperatureInlet/lapseRateProfileTemperatureInletFvPatchScalarField.C
@@ -2,16 +2,16 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright held by original author
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
 
-    OpenFOAM is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation; either version 2 of the License, or (at your
-    option) any later version.
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
     OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -19,237 +19,242 @@ License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenFOAM; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
 
 #include "lapseRateProfileTemperatureInletFvPatchScalarField.H"
-#include "addToRunTimeSelectionTable.H"
+#include "incompressible/turbulenceModel/turbulenceModel.H"
 #include "fvPatchFieldMapper.H"
 #include "volFields.H"
-#include "surfaceFields.H"
+#include "addToRunTimeSelectionTable.H"
+#include "wallFvPatch.H"
+#include "nutkWallFunctionFvPatchScalarField.H"
+
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
+namespace incompressible
+{
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void lapseRateProfileTemperatureInletFvPatchScalarField::checkType()
+{
+    if (isA<wallFvPatch>(patch()))
+    {
+        FatalErrorIn("lapseRateProfileTemperatureInletFvPatchScalarField::checkType()")
+            << "Invalid function specification" << nl
+            << "    Patch type for patch " << patch().name()
+            << " must not be wall" << nl
+            << "    Current patch type is " << patch().type() << nl << endl
+            << abort(FatalError);
+    }
+}
+
+
+void lapseRateProfileTemperatureInletFvPatchScalarField::writeLocalEntries(Ostream& os) const
+{
+    os.writeKeyword("Cmu") << Cmu_ << token::END_STATEMENT << nl;
+    os.writeKeyword("kappa") << kappa_ << token::END_STATEMENT << nl;
+    os.writeKeyword("E") << E_ << token::END_STATEMENT << nl;
+}
+
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
 (
     const fvPatch& p,
-    const DimensionedField<vector, volMesh>& iF
+    const DimensionedField<scalar, volMesh>& iF
 )
 :
-    fixedValueFvPatchVectorField(p, iF),
-    UfreeStream_(0.0),
-    uDirection_(0.0,0.0,0.0),
-    inputWindHeight_Veg_(0),
-    z0_(0),
-    Rd_(0),
-    relativeHeight_(0),
-    firstCellHeight_(0)
-{}
+    fixedInternalValueFvPatchField<scalar>(p, iF),
+    Cmu_(0.09),
+    kappa_(0.41),
+    E_(9.8)
+{
+    checkType();
+}
 
 
 lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
 (
     const lapseRateProfileTemperatureInletFvPatchScalarField& ptf,
     const fvPatch& p,
-    const DimensionedField<vector, volMesh>& iF,
+    const DimensionedField<scalar, volMesh>& iF,
     const fvPatchFieldMapper& mapper
 )
 :
-    fixedValueFvPatchVectorField(ptf, p, iF, mapper),
-    UfreeStream_(ptf.UfreeStream_),
-    uDirection_(ptf.uDirection_),
-    inputWindHeight_Veg_(ptf.inputWindHeight_Veg_),
-    z0_(ptf.z0_),
-    Rd_(ptf.Rd_),
-    relativeHeight_(ptf.relativeHeight_),
-    firstCellHeight_(ptf.firstCellHeight_)
-{}
-
-/* used later in updateCoeffs() --> 
-   this constructor is the one called by simpleFoam */
-lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
-(
-    const fvPatch& p,
-    const DimensionedField<vector, volMesh>& iF,
-    const dictionary& dict
-)
-:
-    fixedValueFvPatchVectorField(p, iF),
-    UfreeStream_(readScalar(dict.lookup("UfreeStream"))),
-    uDirection_(dict.lookup("uDirection")),
-    inputWindHeight_Veg_(readScalar(dict.lookup("inputWindHeight_Veg"))),
-    z0_(readScalar(dict.lookup("z0"))),
-    Rd_(readScalar(dict.lookup("Rd"))),
-    firstCellHeight_(readScalar(dict.lookup("firstCellHeight")))
+    fixedInternalValueFvPatchField<scalar>(ptf, p, iF, mapper),
+    Cmu_(ptf.Cmu_),
+    kappa_(ptf.kappa_),
+    E_(ptf.E_)
 {
-    // Access to face Centers
-    const pointField& faceCenters = patch().Cf();
-    scalarField Dist(faceCenters.size(), 0.0);
-
-    List<pointField> faceCenterField(Pstream::nProcs()); 
-    label nFace = faceCenters.size();
-    reduce(nFace, sumOp<label>()); 
-
-    pointField faceCenterPoints(0);
-    faceCenterField[Pstream::myProcNo()]=patch().Cf();
-    Pstream::gatherList(faceCenterField);
-
-    if(Pstream::master())
-    {	
-	forAll(faceCenterField, procI)
-	{
-		forAll(faceCenterField[procI],pointI)
-		{
-			faceCenterPoints.append(faceCenterField[procI][pointI]);
-		}
-	}
-    }
-    Pstream::scatter(faceCenterPoints);
-
-    forAll(faceCenters, pointI)
-	{
-
-	point currentPt = faceCenters[pointI];
-	// Set to high values
-	Dist[pointI] = GREAT;
-	label minPointId = -1;
-	forAll(faceCenterPoints, pointJ)
-	{
-        //if current point is not the near-wall center point	   
-        if(mag(currentPt.z()-faceCenterPoints[pointJ].z()) > .1 )
- 	   {
-		    scalar checkOrdinate_FC = 0.0, checkOrdinate_CP = 0.0;
-		    // Lookup patch ids and check if its orientation is along west/east/south/north
-		    // If current patch is west_face or east_face, check the Y ordinates   		
-            const word& curPatchName = patch().name();
-		    if(curPatchName == "west_face" || curPatchName == "east_face")
-		    {
-		        checkOrdinate_FC = faceCenterPoints[pointJ].y();
-		        checkOrdinate_CP = currentPt.y();
-		    }
-		    else if(curPatchName == "north_face" || curPatchName == "south_face")
-		    {
-		        checkOrdinate_FC = faceCenterPoints[pointJ].x() ;
-		    	checkOrdinate_CP = currentPt.x();
-		    }
-		    else
-		    {
-         	    FatalErrorIn("lapseRateProfileTemperatureInletFvPatchScalarField")
-     		       << "Boundary condition applied on incorrect patch"
-		       << abort(FatalError);
-    	    }
-            // Look for cells close to x or y coord (depending on the face) of currentPt 
-		    if( mag(checkOrdinate_FC - checkOrdinate_CP) < 100 )
-	            {
-                //if we are close in x or y dimension, 
-                //and closer to the ground than the previous iteration, 
-                //calc and store distance to ground for this face
-		        if( faceCenterPoints[pointJ].z() < Dist[pointI] )
-		        {
-                    //update distance to ground if we are in a lower cell than previous iteration
-			        Dist[pointI] = faceCenterPoints[pointJ].z();
-			        minPointId = pointJ; //point with z nearest to ground (the near-ground cell below currentPt)
-	    	    }
-		    }
-	    }
-	}
-
-	// If no other values are found (assuming the bracket range is sufficient)
-	if(Dist[pointI] == GREAT || minPointId == -1 || Dist[pointI] > currentPt.z() )
-	{
-	    Dist[pointI] = currentPt.z();
-	}
-
-	// Add the half first cell Height read from dictionary
-    //Info<<"firstCellHeight_ = "<<firstCellHeight_<<endl;
- 	
-    Dist[pointI] = currentPt.z() - Dist[pointI] + firstCellHeight_/2;
-	relativeHeight_.setSize(Dist.size());
-    relativeHeight_ = Dist;
-
-    //Info<<"currentPt.z() = "<<currentPt.z()<<endl;    
-    //Info<<"Dist[pointI] = "<<Dist[pointI]<<endl;
-
-	}
-
-    evaluate();
+    checkType();
 }
 
 
 lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
 (
-    const lapseRateProfileTemperatureInletFvPatchScalarField& fcvpvf,
-    const DimensionedField<vector, volMesh>& iF
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
 )
 :
-    fixedValueFvPatchVectorField(fcvpvf, iF),
-    UfreeStream_(fcvpvf.UfreeStream_),
-    uDirection_(fcvpvf.uDirection_),
-    inputWindHeight_Veg_(fcvpvf.inputWindHeight_Veg_),
-    z0_(fcvpvf.z0_),
-    Rd_(fcvpvf.Rd_),
-    relativeHeight_(fcvpvf.relativeHeight_),
-    firstCellHeight_(fcvpvf.firstCellHeight_)
-{}
+    fixedInternalValueFvPatchField<scalar>(p, iF, dict),
+    Cmu_(dict.lookupOrDefault<scalar>("Cmu", 0.09)),
+    kappa_(dict.lookupOrDefault<scalar>("kappa", 0.41)),
+    E_(dict.lookupOrDefault<scalar>("E", 9.8))
+{
+    checkType();
+}
+
+
+lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
+(
+    const lapseRateProfileTemperatureInletFvPatchScalarField& ewfpsf
+)
+:
+    fixedInternalValueFvPatchField<scalar>(ewfpsf),
+    Cmu_(ewfpsf.Cmu_),
+    kappa_(ewfpsf.kappa_),
+    E_(ewfpsf.E_)
+{
+    checkType();
+}
+
+
+lapseRateProfileTemperatureInletFvPatchScalarField::lapseRateProfileTemperatureInletFvPatchScalarField
+(
+    const lapseRateProfileTemperatureInletFvPatchScalarField& ewfpsf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedInternalValueFvPatchField<scalar>(ewfpsf, iF),
+    Cmu_(ewfpsf.Cmu_),
+    kappa_(ewfpsf.kappa_),
+    E_(ewfpsf.E_)
+{
+    checkType();
+}
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-//called by simpleFoam
 void lapseRateProfileTemperatureInletFvPatchScalarField::updateCoeffs()
 {
-    scalar ustar = UfreeStream_*0.41/Foam::log((inputWindHeight_Veg_)/z0_);
-    scalar ucalc(0.0);
-    vectorField Up(patch().Cf().size(), vector(0.0, 0.0, 0.0));
-    
-    // Loop over all the faces in that patch
-    forAll(Up, faceI )
+    if (updated())
     {
-        // relative height from ground for face lists
-        scalar& AGL = relativeHeight_[faceI];
-
-        //Apply the log profile
-        ucalc = ustar/0.41*Foam::log((AGL)/z0_);
-        Up[faceI] = ucalc*uDirection_;
+        return;
     }
 
-    operator==(Up);
+    const label patchI = patch().index();
 
+    const turbulenceModel& turbulence =
+        db().lookupObject<turbulenceModel>("turbulenceModel");
+    const scalarField& y = turbulence.y()[patchI];
+
+    IOdictionary transportProperties
+    (
+        IOobject
+        (
+            "transportProperties",
+            turbulence.U().mesh().time().constant(),
+            turbulence.U().mesh(),
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+    dimensionedScalar rho(transportProperties.lookup("rho"));
+
+    const scalar Cmu25 = pow025(Cmu_);
+    const scalar Cmu75 = pow(Cmu_, 0.75);
+
+    volScalarField& G =
+        const_cast<volScalarField&>
+        (
+            db().lookupObject<volScalarField>
+            (
+                turbulence.type() + ".G"
+            )
+        );
+
+    DimensionedField<scalar, volMesh>& epsilon =
+        const_cast<DimensionedField<scalar, volMesh>&>
+        (
+            dimensionedInternalField()
+        );
+
+    const tmp<volScalarField> tk = turbulence.k();
+    const volScalarField& k = tk();
+    const scalarField& kw = k.boundaryField()[patchI];
+
+    const tmp<volScalarField> tnu = turbulence.nu();
+    const scalarField& nuw = tnu().boundaryField()[patchI];
+
+    const tmp<volScalarField> tnut = turbulence.nut();
+    const volScalarField& nut = tnut();
+    const scalarField& nutw = nut.boundaryField()[patchI];
+
+    const fvPatchVectorField& Uw = turbulence.U().boundaryField()[patchI];
+
+    const scalarField magGradUw(mag(Uw.snGrad()));
+
+    const scalar rhow(rho.value());
+    const scalarField muw(nuw*rhow);
+    scalarField yStarv(muw.size(),11.24);
+    const scalarField yv(nuw*yStarv/(Cmu25*pow(kw,0.5)));
+
+
+    // Set epsilon and G
+    forAll(nutw, faceI)
+    {
+        label faceCellI = patch().faceCells()[faceI];
+
+        epsilon[faceCellI] =k[faceCellI] *(2*muw[faceI]/(rhow*yv[faceI])
+            +pow(k[faceCellI],0.5)*log(2.0*y[faceI]/yv[faceI])/(kappa_*pow(Cmu_,-0.75)))/(2.0*y[faceI]);
+        scalar tw=rhow*(nutw[faceI] + nuw[faceI])*magGradUw[faceI];
+
+        G[faceCellI] = pow(tw,2)*log(2*y[faceI]/yv[faceI])/(kappa_*2.0*y[faceI]*rhow*Cmu25*pow(k[faceCellI],0.5));
+    }
+
+    fixedInternalValueFvPatchField<scalar>::updateCoeffs();
+
+    // TODO: perform averaging for cells sharing more than one boundary face
 }
 
-// Write
+
+void lapseRateProfileTemperatureInletFvPatchScalarField::evaluate
+(
+    const Pstream::commsTypes commsType
+)
+{
+    fixedInternalValueFvPatchField<scalar>::evaluate(commsType);
+}
+
+
 void lapseRateProfileTemperatureInletFvPatchScalarField::write(Ostream& os) const
 {
-    fvPatchVectorField::write(os);
-    os.writeKeyword("UfreeStream")
-        << UfreeStream_ << token::END_STATEMENT << nl;
-    os.writeKeyword("uDirection")
-        << uDirection_ << token::END_STATEMENT << nl;
-    os.writeKeyword("inputWindHeight_Veg")
-        << inputWindHeight_Veg_ << token::END_STATEMENT << nl;
-    os.writeKeyword("z0")
-        << z0_ << token::END_STATEMENT << nl;
-    os.writeKeyword("Rd")
-        << Rd_ << token::END_STATEMENT << nl;
-    os.writeKeyword("firstCellHeight")
-	<< firstCellHeight_ << token::END_STATEMENT << nl;
+    fixedInternalValueFvPatchField<scalar>::write(os);
+    writeLocalEntries(os);
     writeEntry("value", os);
 }
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-makePatchTypeField(fvPatchVectorField, lapseRateProfileTemperatureInletFvPatchScalarField);
+makePatchTypeField
+(
+    fvPatchScalarField,
+    lapseRateProfileTemperatureInletFvPatchScalarField
+);
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
+} // End namespace incompressible
 } // End namespace Foam
 
 // ************************************************************************* //
